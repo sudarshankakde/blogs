0bd185a37b2e9d42bd8cfcab3eb4c12afd3ae8db
Sudarshan kakde <60603199+sudarshankakde@users.noreply.github.com>
2023-12-19 20:10:33 +0530

    requirment updated

--- sid/apiUrls.py
+++ sid/apiUrls.py
@@ -4,7 +4,7 @@ from blog import views as blogViews
 
 urlpatterns = [
     # apis for extenal use
-    path('Blogs',blogViews.BlogsApi, name='AllBlogsApi'),
+    path('blogs',blogViews.BlogsApi, name='AllBlogsApi'),
     path('detail/<str:slug>',blogViews.detailApi, name='detailApi'),
     path('projects',views.projectsApi, name=''),
     path('newsletter',views.newsletterSubscription, name=''),
