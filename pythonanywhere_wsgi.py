3329962557e9adc64031c5a6fc329cc15b26b241
sudarshankakde <sudarshankakde1111@gmail.com>
2026-07-21 21:35:02 +0530

    Make pythonanywhere_wsgi.py path dynamic

--- pythonanywhere_wsgi.py
+++ pythonanywhere_wsgi.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 # Path to your project directory on PythonAnywhere
-path = '/home/sudarshankakde/blogs'
+path = os.path.dirname(os.path.abspath(__file__))
 if path not in sys.path:
     sys.path.append(path)
 
